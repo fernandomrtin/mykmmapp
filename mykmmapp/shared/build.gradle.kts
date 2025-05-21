c68d41786382561688faa26996483515e709d1d2
fernandomm92 <fernandomm92@gmail.com>
2025-05-21 16:03:26 +0200

    podfile

--- mykmmapp/shared/build.gradle.kts
+++ mykmmapp/shared/build.gradle.kts
@@ -4,6 +4,19 @@ plugins {
     id("com.android.library") // Solo si necesitas Android
 }
 
+android {
+    compileSdk = 34  // Usa la última versión estable (o la que necesites)
+
+    defaultConfig {
+        minSdk = 21  // Mínimo SDK soportado
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
 kotlin {
     androidTarget()
     iosX64()
